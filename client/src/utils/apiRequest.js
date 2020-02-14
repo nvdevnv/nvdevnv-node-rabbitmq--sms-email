cea5b9482a0238c7f49cbc6217713e47b1bf9e7b
Nitin Verma <nvdevnv@gmail.com>
2020-02-14 18:38:20 +0530

    Updated API_URL.

--- client/src/utils/apiRequest.js
+++ client/src/utils/apiRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = `http://localhost:3001/notifications/`;
+const API_URL = `http://${process.env.API_URL ||
+  "localhost:3001"}/notifications/`;
 
 const sendMessage = async data => {
   const auxOptions = {
